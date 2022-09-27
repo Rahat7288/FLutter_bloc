a7b30d776910288ce4a898678735e05aa5072ee9
rahat <rahatislamakash@gmail.com>
2022-09-27 12:24:02 +0600

    learning about the bloc listener and the blocConsumer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_conc/cubits/counter/counter_cubit.dart';
+import 'package:bloc_conc/other_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -28,6 +29,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/**
+ * 
+ * if we use the BlocListener we have to use the BlocBuilder If We use BlocConsumer we don't need to 
+ * use the BlocBuilder
+ */
+
 class MyhomePage extends StatelessWidget {
   const MyhomePage({super.key});
 
@@ -35,55 +42,77 @@ class MyhomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     /**
      * if we use the BlocBuilder we don't need to use blocProvider listen to be true
-     * we can access the variabls by using the state. operator
+     * we can access the variabls by using the
      */
-    return BlocBuilder<CounterCubit, CounterState>(
-      builder: (context, state) {
-        return Scaffold(
-          body: Center(
-            child: Text(
-              /**
-               * accessing the counter from CounterCubit state
-               * to display the value on the ui we have to implement the (listen:  true) to manupulate the 
-               * ui output
-               * listen helps ui to rebuild
-               */
-              '${state.counter}',
-              style: TextStyle(fontSize: 52.0),
-            ),
-          ),
-          floatingActionButton: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              FloatingActionButton(
-                onPressed: () {
-                  /**
-                   * calling the function from CounterCubit cubit.dart file
-                   * incriment function
-                   */
-                  BlocProvider.of<CounterCubit>(context).increment();
-                },
-                child: Icon(Icons.add),
-                heroTag: 'increment',
-              ),
-              SizedBox(
-                width: 10.0,
-              ),
-              FloatingActionButton(
-                onPressed: () {
-                  /**
-                   * calling the function from CounterCubit cubit.dart file
-                   * decriment function
-                   */
-                  BlocProvider.of<CounterCubit>(context).decriment();
-                },
-                child: Icon(Icons.remove),
-                heroTag: 'decrement',
+
+    return BlocListener<CounterCubit, CounterState>(
+      listener: (context, state) {
+        // TODO: implement listener
+         if(state.counter == 3){
+              showDialog(
+                context: context,
+                 builder: (context) => AlertDialog(
+                  content: Text('Counter in ${state.counter}'),
+                 )
+                 );
+            }else if(state.counter == -1){
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                return OtherPage();
+              }
               )
-            ],
-          ),
-        );
+              );
+            }
       },
+      child: Scaffold(
+        body: BlocBuilder<CounterCubit, CounterState>(
+          builder: (context, state) {
+
+           
+            return Center(
+              child: Text(
+                /**
+                             * accessing the counter from CounterCubit state
+                             * to display the value on the ui we have to implement the (listen:  true) to manupulate the 
+                             * ui output
+                             * listen helps ui to rebuild
+                             */
+                '${state.counter}',
+                style: TextStyle(fontSize: 52.0),
+              ),
+            );
+          },
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                /**
+                       * calling the function from CounterCubit cubit.dart file
+                       * incriment function
+                       */
+                BlocProvider.of<CounterCubit>(context).increment();
+              },
+              child: Icon(Icons.add),
+              heroTag: 'increment',
+            ),
+            SizedBox(
+              width: 10.0,
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                /**
+                       * calling the function from CounterCubit cubit.dart file
+                       * decriment function
+                       */
+                BlocProvider.of<CounterCubit>(context).decriment();
+              },
+              child: Icon(Icons.remove),
+              heroTag: 'decrement',
+            )
+          ],
+        ),
+      ),
     );
   }
 }
