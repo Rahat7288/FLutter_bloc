b728da7b99ff6e28ae1b151253f4ff2ba108294e
rahat <rahatislamakash@gmail.com>
2022-09-27 11:55:26 +0600

    implimenting the BlocProvider to access the function and the variables

--- lib/cubits/counter/counter_cubit.dart
+++ lib/cubits/counter/counter_cubit.dart
@@ -6,7 +6,15 @@ part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState.initial());
+  /**
+   * in the cubit.dart file we diclear all the functions 
+   * we use emit to manipulate the state.dart file variables
+   */
   void increment(){
-    
+    emit(state.copyWith(counter: state.counter + 1));
+  }
+
+  void decriment(){
+    emit(state.copyWith(counter: state.counter - 1)); 
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_conc/cubits/counter/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,17 +9,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'my counter cubit',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
+    /**
+     * wrap with BlocProver to access the CounterCubit variables and functions
+     */
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'my counter cubit',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyhomePage(),
       ),
-      home: const MyhomePage(),
     );
   }
 }
@@ -30,24 +36,43 @@ class MyhomePage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Text(
-          '0',
+          /**
+           * accessing the counter from CounterCubit state
+           * to display the value on the ui we have to implement the (listen:  true) to manupulate the 
+           * ui output
+           */
+          '${BlocProvider.of<CounterCubit>(context, listen: true).state.counter}',
           style: TextStyle(fontSize: 52.0),
         ),
       ),
-      floatingActionButton:  Row(
+      floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: (){},
+            onPressed: () {
+              /**
+               * calling the function from CounterCubit cubit.dart file
+               * incriment function
+               */
+              BlocProvider.of<CounterCubit>(context).increment();
+            },
             child: Icon(Icons.add),
             heroTag: 'increment',
-            ),
-            SizedBox(width: 10.0,),
-            FloatingActionButton(
-              onPressed: (){},
-              child: Icon(Icons.remove),
-              heroTag: 'decrement',
-              )
+          ),
+          SizedBox(
+            width: 10.0,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              /**
+               * calling the function from CounterCubit cubit.dart file
+               * decriment function
+               */
+              BlocProvider.of<CounterCubit>(context).decriment();
+            },
+            child: Icon(Icons.remove),
+            heroTag: 'decrement',
+          )
         ],
       ),
     );
